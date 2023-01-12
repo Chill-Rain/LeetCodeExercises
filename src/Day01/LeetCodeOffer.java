20290d8f5f394b706e3f7ae033c4601b44489079
Chill—Rain <2135566232@qq.com>
2023-01-12 16:28:14 +0800

    第一天

--- src/Day01/LeetCodeOffer.java
+++ src/Day01/LeetCodeOffer.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
  * @author ChillRain 2023 01 11
  */
 public class LeetCodeOffer {
-
+    /**
+     * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+     * 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+     */
     class Solution {
         public int findRepeatNumber(int[] nums) {
             Integer number = null; //用于记录重复数字
